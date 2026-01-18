afccd5721ece982c2cb009c5eb94d41b84b55eed
0x584758 <3757549181@qq.com>
2026-01-18 17:27:07 +0800

    创建项目“学灵AI”

---
+++ src/main/java/com/hope/xueling/XueLingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.hope.xueling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XueLingPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XueLingPlatformApplication.class, args);
+    }
+
+}
