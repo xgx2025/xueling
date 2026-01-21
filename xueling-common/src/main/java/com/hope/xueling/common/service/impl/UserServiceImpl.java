c9b6d9bebeb9c6d69a99bcf4931d4a9e3d4be148
谢光益 <3791158284@qq.com>
2026-01-21 10:45:24 +0800

    增加正则表达式校验：根据用户邮箱和用户手机号分别查用户信息

--- xueling-common/src/main/java/com/hope/xueling/common/service/impl/UserServiceImpl.java
+++ xueling-common/src/main/java/com/hope/xueling/common/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.hope.xueling.common.service.impl;
 
 import com.hope.xueling.common.domain.entity.User;
+import com.hope.xueling.common.exception.BaseException;
 import com.hope.xueling.common.mapper.UserMapper;
 import com.hope.xueling.common.service.UserService;
 import org.springframework.stereotype.Service;
@@ -13,7 +14,7 @@ public class UserServiceImpl implements UserService {
 
     //注入依赖
 
-     private final UserMapper userMapper;//用户Mapper
+    private final UserMapper userMapper;//用户Mapper
 
     public UserServiceImpl(UserMapper userMapper) {
         this.userMapper = userMapper;
@@ -21,11 +22,18 @@ public class UserServiceImpl implements UserService {
 
     /**
      * 根据用户邮箱查用户信息
+     * @param email 邮箱
+     * @return User 用户实体类
      */
     @Override
     public User getUserByEmail(String email) {
         //如果邮箱不为空，则返回邮箱对应的用户信息，否则返回null
         if (email != null) {
+            //正则表达式校验
+            if (!email.matches("^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*\\.[a-zA-Z0-9]{2,6}$")) {
+                //邮箱格式错误异常
+                throw new BaseException("邮箱格式错误");
+            }
             return userMapper.selectByEmail(email);
         }
         return null;
@@ -40,8 +48,13 @@ public class UserServiceImpl implements UserService {
     public User getUserByPhone(String phone) {
         //如果手机号不为空，则返回手机号对应的用户信息，否则返回null
         if (phone != null) {
+            //正则表达式校验
+            if (!phone.matches("^1[3456789]\\d{9}$")) {
+                //手机号格式错误异常
+                throw new BaseException("手机号格式错误");
+            }
             return userMapper.selectByPhone(phone);
         }
         return null;
     }
- }
+}
